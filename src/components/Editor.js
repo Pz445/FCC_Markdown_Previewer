7a65d8647e2ee472c30bf99032eee0cd65cd1079
Pat P <sell445@gmail.com>
2019-03-19 10:32:55 -0400

    fixed editor id

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types'
 
 const Editor = (props) => {
   return (
-    <div id="editor">
+    <div id="editor-wrapper">
       <div id="editor-bar"><h2 className="div-title">Editor</h2></div>
-      <textarea onChange={props.onChange} value={props.value}></textarea>
+      <textarea onChange={props.onChange} value={props.value} id="editor"></textarea>
     </div>
   )
 }
